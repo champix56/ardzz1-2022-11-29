671cceb1a55d34a42070dc44d8c54d07c362ac89
champix56 <desorbaix@free.fr>
2022-11-29 16:26:48 +0100

    base d'un program arduino ino

---
+++ redlight/redlight.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Setup");
+
+}
+
+void loop() {
+  Serial.println("loop");
+  // put your main code here, to run repeatedly:
+  delay(50000);
+}
