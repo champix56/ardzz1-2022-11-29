2db75bd33380bf8c00456a7e94d80278c4696e72
champix56 <desorbaix@free.fr>
2022-11-30 08:39:45 +0100

    passage du code de feux en fonction

--- redlight/redlight.ino
+++ redlight/redlight.ino
@@ -3,32 +3,38 @@ void setup() {
   Serial.begin(9600);
   Serial.println("Setup");
   //def des pullup res
-  pinMode(8,OUTPUT);
-  pinMode(9,OUTPUT);
-  pinMode(10,OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
 }
 
 void loop() {
   Serial.println("loop");
   // put your main code here, to run repeatedly:
+  initPassagePieton(50000);
+}
+/**
+   declenchement pour le passage pieton de sequence des feux
+   @params waitTime {uint6_t} temps d'attente au vert pour le pieton en ms
+*/
+void initPassagePieton(uint16_t waitTime) {
   //extinction rouge
-  digitalWrite(10,LOW);
-  
+  digitalWrite(10, LOW);
   //allumage vert
-  digitalWrite(8,HIGH);
-    //attente
-  delay(1000);
-    //extinction vert
-  digitalWrite(8,LOW);
-    //allumage orange
-  digitalWrite(9,HIGH);
+  digitalWrite(8, HIGH);
   //attente
   delay(1000);
-    //extinction orange
-  digitalWrite(9,LOW);
-    //allumage rouge
-  digitalWrite(10,HIGH);
- 
-  
-  delay(50000);
+  //extinction vert
+  digitalWrite(8, LOW);
+  //allumage orange
+  digitalWrite(9, HIGH);
+  //attente
+  delay(1000);
+  //extinction orange
+  digitalWrite(9, LOW);
+  //allumage rouge
+  digitalWrite(10, HIGH);
+
+  delay(waitTime);
+
 }
